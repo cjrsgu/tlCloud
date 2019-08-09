2b5033cedd9724ba849d422ab748b0325599a96e
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-08-09 14:29:47 -0700

    create auth

--- src/index.js
+++ src/index.js
@@ -1,18 +1,76 @@
+require('dotenv').config();
 const Client = require('./Client.js');
 
 const client = new Client();
 
-// Testing execute
-console.log('execute', client.execute({
-  '@type': 'getTextEntities',
-  text: '@telegram /test_command https://telegram.org telegram.me',
-}));
+while (true) {
+  const event = client.receive();
+  console.log(event);
+  if (event) {
+    if (event['@type'] === 'updateConnectionState') {
+      const connectionState = event.state;
+      if (connectionState['@type'] === 'connectionStateReady') {
+        client.send({
+          '@type': 'getChats',
+          limit: 10,
+        });
+      }
+    }
 
-// Testing send
-client.send({ '@type': 'getAuthorizationState' });
 
-// Testing receive
-console.log('receive', client.receive());
+    if (event['@type'] === 'updateAuthorizationState') {
+      const authState = event.authorization_state;
+      if (authState['@type'] === 'authorizationStateClosed') {
+        break;
+      }
 
-// Destroy client
+      if (authState['@type'] === 'authorizationStateWaitTdlibParameters') {
+        client.send({
+          '@type': 'setTdlibParameters',
+          parameters: {
+            database_directory: 'tdlib',
+            use_message_database: true,
+            use_secret_chats: true,
+            api_id: process.env.API_ID,
+            api_hash: process.env.API_HASH,
+            system_language_code: 'en',
+            device_model: 'tlCloud',
+            system_version: 'Windows',
+            application_version: '1.0',
+            enable_storage_optimizer: true,
+          },
+        });
+      }
+
+      if (authState['@type'] === 'authorizationStateWaitEncryptionKey') {
+        client.send({
+          '@type': 'setDatabaseEncryptionKey',
+          new_encryption_key_: process.env.ENCRYPTION_KEY,
+        });
+      }
+
+      if (authState['@type'] === 'authorizationStateWaitPhoneNumber') {
+        client.send({
+          '@type': 'setAuthenticationPhoneNumber',
+          phone_number: process.env.PHONE,
+        });
+      }
+
+      if (authState['@type'] === 'authorizationStateWaitCode') {
+        client.send({
+          '@type': 'checkAuthenticationCode',
+          code: '',
+        });
+      }
+
+      if (authState['@type'] === 'authorizationStateWaitPassword') {
+        client.send({
+          '@type': 'checkAuthenticationPassword',
+          password: process.env.PASSWORD,
+        });
+      }
+    }
+  }
+}
+
 client.destroy();
